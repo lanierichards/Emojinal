012e320eac7508f716ccaf567b6e372eddd989bf
lanierichards <hallesport@gmail.com>
2019-07-12 09:32:11 -0700

    Second Commit

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = ["🥳": "birthday emoji", "🤩": "starry eyes emoji"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "TITLEHERE", message: "DOTHISLATER", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "YAY!", message: "IT'S YOUR BIRTHDAY!", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
+        
 
     }
     
