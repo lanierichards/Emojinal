543ce21aca51928f677ed96e9ea2c86ebcce0c4b
lanierichards <hallesport@gmail.com>
2019-07-12 10:41:36 -0700

    Third Commit

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     let emojis = ["🥳": "birthday emoji", "🤩": "starry eyes emoji"]
+    var customMessages = ["birthday emoji" : ["ITS YA BIRTHDAY", "PARTY GIRL", "PARTY IT UP","ITS A CELEBRATION,C'MON!", "HAPPY BIRTHDAY!", "GO OUT AND CELEBRATE", "HAVING A GOOD TIME, HAVING A GOOD TIME"], "starry eyes emoji" : ["YOU LOOK SO PRETTY", "SLAY QUEEN", "YOU LIGHT UP MY WORLD", "YOU GO GIRL", "WOW WOW WOW", "🔥🔥🔥", "KARLIE KLOSS WOULD BE PROUD OF YOUR FASHION"]]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +20,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "YAY!", message: "IT'S YOUR BIRTHDAY!", preferredStyle: UIAlertController.Style.alert)
+        var number = Int.random(in: 0 ... 6)
+        let selectEmotion = sender.titleLabel?.text
+        
+        let emojiMessage = customMessages[emojis[selectEmotion!]!]?[number]
+        let alertController = UIAlertController(title: "YAY!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
-        
 
     }
     
     @IBAction func partyEmoji(_ sender: Any) {
-        
 
     }
     
